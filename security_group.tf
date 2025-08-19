505c8db3d5b2426eb7f78612a22c80475ea435f6
boinpellypranavi-pixel <boinpelly.pranavi@gmail.com>
2025-08-19 19:28:41 -0400

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "alb_sg_pranavi" {
+  name        = "alb-pranavi"
+  description = "Allow HTTPS"
+  vpc_id      = aws_vpc.pranavi_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
